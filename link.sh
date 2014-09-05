5dea33b32329d1107b3266bc5c6a3628d3ee9485
David Ulrich <dulrich@atomicinfotech.com>
2014-09-05 15:20:10 -0500

    improve linking for aliases and irssi scripts

--- link.sh
+++ link.sh
@@ -1,3 +1,8 @@
-# add source scripts/aliases.sh
-
+# link .bash_aliases to the repo aliases file
+rm ~/.bash_aliases
 ln -s ~/scripts/aliases.sh ~/.bash_aliases
+
+# link irssi scripts into the repo
+mkdir -p ~/.irssi
+rm ~/.irssi/scripts
+ln -s ~/scripts/irssi_scripts ~/.irssi/scripts
