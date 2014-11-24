4c8ab539d967f8160134d711aacc9938761a2207
David Ulrich <dulrich@atomicinfotech.com>
2014-11-24 11:17:00 -0600

    sloppy, sloppy...

--- aliases.sh
+++ aliases.sh
@@ -41,7 +41,7 @@ alias rdesktop="rdesktop -g $rd_res"
 alias daylog="~/scripts/daylog.sh"
 alias dl="~/scripts/daylog.sh"
 alias life="~/scripts/daylog.sh -f life"
-alias life="~/scripts/daylog.sh -f food"
+alias food="~/scripts/daylog.sh -f food"
 
 # defarg args which default
 defarg() {
