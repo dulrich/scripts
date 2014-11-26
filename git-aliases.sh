5f473c8200f2e254ee7d767d8b5978afed9a9316
David Ulrich <dulrich@atomicinfotech.com>
2014-11-25 19:27:17 -0600

    ...

--- git-aliases.sh
+++ git-aliases.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# git-aliases.sh: shorten common git tasks
+# Copyright 2013 - 2014 David Ulrich
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 alias s="git status"
 function a {
 	git add $(defarg "$*" '@' '.')
--- link.sh
+++ link.sh
@@ -1,3 +1,19 @@
+# daylog.sh: track what happens at a certain time, and view previous logs
+# Copyright 2013 - 2014 David Ulrich
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # link .bash_aliases to the repo aliases file
 rm ~/.bash_aliases
 ln -s ~/scripts/aliases.sh ~/.bash_aliases
