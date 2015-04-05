116756581613c85a6a3bb5ccb515914887edc6f4
David Ulrich <dulrich@atomicinfotech.com>
2015-04-05 01:29:32 -0500

    additional editor setting tweaks

---
+++ textadpet/properties.lua
@@ -0,0 +1,9 @@
+buffer.use_tabs = true
+buffer.tab_width = 4
+
+buffer.property['fold'] = '0'
+
+-- buffer.scroll_width_tracking = true -- no effect?
+buffer.v_scroll_bar = false
+buffer.view_ws = buffer.WS_VISIBLEALWAYS
+
