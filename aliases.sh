543d4b7d2af3f35ede78daede33bea4f047b066f
David Ulrich <dulrich@atomicinfotech.com>
2015-07-10 14:42:38 -0700

    more flexible production commands

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # aliases.sh: shorten common tasks
-# Copyright 2013 - 2015 David Ulrich
+# Copyright 2013 - 2015  David Ulrich
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
