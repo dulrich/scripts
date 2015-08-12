bd57ca5011d26f61f5f26441f296ae0d3bc46fa6
David Ulrich <dulrich@atomicinfotech.com>
2015-08-11 22:03:23 -0700

    extra crouton aliasing steps

---
+++ crouton_aliases.sh
@@ -0,0 +1,10 @@
+. ssh-find-agent.sh
+
+ssh-find-agent -a
+if [ -z "$SSH_AUTH_SOCK" ]
+then
+   eval $(ssh_agent) > /dev/null
+   ssh-add -l >/dev/null || alias ssh='ssh-add -l >/dev/null || ssh-add && unalias ssh; ssh'
+fi
+
+source $home/scripts/aliases.sh
