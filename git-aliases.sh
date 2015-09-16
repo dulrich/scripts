d79ba2622c526127ca26bb6a4b5015263a99a083
David Ulrich <dulrich@atomicinfotech.com>
2015-09-16 13:51:28 -0700

    more concise git status, but keep branch info displayed

--- git-aliases.sh
+++ git-aliases.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-alias s="git status"
+alias s="git status -bs"
 a () {
 	git add $(defarg "$*" '@' '.')
 }
