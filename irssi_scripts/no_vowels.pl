8ef05212a9d0a660641ac846f224aa47ee8e76eb
David Ulrich <dulrich@atomicinfotech.com>
2015-09-17 15:45:58 -0700

    add a stupid new script that strips vowels out of irssi

---
+++ irssi_scripts/no_vowels.pl
@@ -0,0 +1,47 @@
+# expando.pl: turn short sequences into longer statments
+# Copyright (C) 2014 - 2015  David Ulrich
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+
+use vars qw($VERSION %IRSSI);
+
+$VERSION = '0.0.1';
+%IRSSI = (
+	authors		=> 'David Ulrich',
+	contact		=> 'david@ulrichdev.com',
+	name		=> 'expando',
+	description	=> 'Strips vowels out of messages started with $$.',
+	license		=> 'BSD',
+	url		=> 'none',
+);
+
+sub do_expansion () {
+	my ($line, $server_rec, $wi_item_rec) = @_;
+	
+	
+	my ($vn) = $line =~ /^\$\$/;
+	
+	if ($vn) {
+		$line =~ s/^\$\$(\s+)?//;
+		
+		$line =~ s/(?:[aeiou]+)([b-z^aeiou])/$1/gi;
+        $line =~ s/([b-z^aeiou])(?:[aeiou]+)/$1/gi;
+	}
+	
+	Irssi::signal_continue($line, $server_rec, $wi_item_rec);
+}
+
+Irssi::signal_add_first('send text', "do_expansion");
