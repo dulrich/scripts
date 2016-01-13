7c8a329d02b35ada0164a1a0bbac348f8dc1c753
David Ulrich <dvulrich47@gmail.com>
2016-01-12 20:57:30 -0800

    ssh agent was failing because of a typo

--- crouton_aliases.sh
+++ crouton_aliases.sh
@@ -1,9 +1,8 @@
 . $HOME/scripts/ssh-find-agent.sh
 
 ssh-find-agent -a
-if [ -z "$SSH_AUTH_SOCK" ]
-then
-   eval $(ssh_agent) > /dev/null
+if [ -z "$SSH_AUTH_SOCK" ]; then
+   eval $(ssh-agent) > /dev/null
    ssh-add -l >/dev/null || alias ssh='ssh-add -l >/dev/null || ssh-add && unalias ssh; ssh'
 fi
 
