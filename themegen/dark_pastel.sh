c20dca42d466c4b0e26e8a65ffbfbbd125f48c96
David Ulrich <david@ulrichdev.com>
2024-11-27 17:21:58 -0600

    theme switching work

---
+++ themegen/dark_pastel.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+FOREGROUND="#c5c8c6ff"
+BACKGROUND="#000000bb"
+CURSOR="#c5c8c6ff"
+TRANSPARENCY="#00000000"
+TRANSPARENCY_XSTR="rgba:0000/0000/0000/cccc"
+
+FONTSIZE_SMALL="12"
+FONTSIZE_MAIN="18"
+
+PURE_BLACK="#000000ff"
+PURE_WHITE="#ff0000ff"
+
+BLACK_LITE="#373b41ff" # color 8
+BLACK_DARK="#282a2eff" # color 0
+
+RED_LITE="#cc6666ff" # color 9
+RED_DARK="#a54242ff" # color 1
+
+GREEN_LITE="#8abd68ff" # color 10
+GREEN_DARK="#649440ff" # color 2
+
+YELLOW_LITE="#f0c674ff" # color 11
+YELLOW_DARK="#de935fff" # color 3
+
+BLUE_LITE="#81a2beff" # color 12
+BLUE_DARK="#5f819dff" # color 4
+
+MAGENTA_LITE="#b294bbff" # color 13
+MAGENTA_DARK="#85678fff" # color 5
+
+CYAN_LITE="#8abeb7ff" # color 14
+CYAN_DARK="#5e8d87ff" # color 6
+
+WHITE_LITE="#c5c8c6ff" # color 15
+WHITE_DARK="#707880ff" # color 7
+
+
